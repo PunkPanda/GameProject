98d2d262e88ebb5343ed77015c98c3ad7cb3e653
HungryHeadcrab <hungryheadcrab@gmail.com>
2014-02-12 00:03:46 -0800

    deleted

--- Game/Game/WndProc.h
+++
@@ -1,8 +0,0 @@
-#ifndef WINDOWS_H
-	#define WINDDOWS_H
-	#include <Windows.h>
-#endif
-
-LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);               // Declaration For WndProc
-
-extern bool    active;                                // Window Active Flag Set To TRUE By Default
