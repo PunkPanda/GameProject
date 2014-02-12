64fa737f8f4e5754526bc7bb887a33c2d696f8da
HungryHeadcrab <hungryheadcrab@gmail.com>
2014-02-12 05:45:13 -0800

    Bunch of shit

--- Game/Assets/Scripts/CameraFollow.cs
+++ Game/Assets/Scripts/CameraFollow.cs
@@ -1,10 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Camera : MonoBehaviour 
+public class CameraFollow : MonoBehaviour 
 {
-  public float xMargin = 1f;    // Distance in the x axis the player can move before the camera follows.
-  public float yMargin = 1f;    // Distance in the y axis the player can move before the camera follows.
+  public float xMargin = 30f;    // Distance in the x axis the player can move before the camera follows.
+  public float yMargin = 30f;    // Distance in the y axis the player can move before the camera follows.
   public float xSmooth = 8f;    // How smoothly the camera catches up with it's target movement in the x axis.
   public float ySmooth = 8f;    // How smoothly the camera catches up with it's target movement in the y axis.
   public Vector2 maxXAndY;    // The maximum x and y coordinates the camera can have.
---
+++ Game/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour 
+{
+  public float maxVelocity = 10f;
+  public float maxSprint = 15f;
+  public float sprintBoost = 1.5f;
+
+		// Update is called once per frame
+	void Update () 
+	{
+
+	}
+
+  void FixedUpdate()
+  {
+    float h = Input.GetAxis ("Horizontal"); // Value of horizontal movement input
+    float s = Input.GetAxis ("Sprint");     // Value of sprint key input
+
+    if (s > 0) 
+    {
+      Debug.Log("Sprinting");
+
+        // Move player (sprint)
+      if (h * rigidbody2D.velocity.x < maxSprint)
+      {
+        rigidbody2D.velocity += Vector2.right * (h * sprintBoost);
+      }
+
+      if (Mathf.Abs (rigidbody2D.velocity.x) > maxSprint) 
+      {
+        rigidbody2D.velocity = new Vector2 (Mathf.Sign (rigidbody2D.velocity.x) * maxVelocity, rigidbody2D.velocity.y);
+      }
+    }
+    else 
+    {
+      Debug.Log("Not sprinting");
+
+        // Move player (no sprint)
+      if (h * rigidbody2D.velocity.x < maxVelocity) 
+      {
+        rigidbody2D.velocity += Vector2.right * h;
+      }
+
+      if (Mathf.Abs (rigidbody2D.velocity.x) > maxVelocity) 
+      {
+        rigidbody2D.velocity = new Vector2 (Mathf.Sign (rigidbody2D.velocity.x) * maxVelocity, rigidbody2D.velocity.y);
+      }
+    }
+
+    if (Mathf.Abs(h) < 1) 
+    {
+      rigidbody2D.velocity = new Vector2 (0, rigidbody2D.velocity.y);
+    }
+  }
+}
