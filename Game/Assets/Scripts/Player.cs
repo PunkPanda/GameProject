ef3e9412f5d3c0e1627ded3fd401e342f23b2995
HungryHeadcrab <hungryheadcrab@gmail.com>
2014-02-12 00:12:49 -0800

    Unity Build

---
+++ Game/Assets/Scripts/Player.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour 
+{
+		// Update is called once per frame
+	void Update () 
+	{
+
+	}
+
+  void FixedUpdate()
+  {
+    float h = Input.GetAxis ("Horizontal");
+
+    rigidbody2D.AddForce (Vector2.right * h * 10);
+  }
+}
