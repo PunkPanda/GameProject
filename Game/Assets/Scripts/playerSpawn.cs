12839b69f641a0bf848c909f1ddd1c5d2538b290
HungryHeadcrab <hungryheadcrab@gmail.com>
2014-03-18 08:24:52 -0700

    Added playerSpawn

---
+++ Game/Assets/Scripts/playerSpawn.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerSpawn : MonoBehaviour {
+
+	public Transform Player;
+
+	// Use this for initialization
+	void Awake () 
+	{
+		Object playah;
+
+		playah = Instantiate(Player);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}
